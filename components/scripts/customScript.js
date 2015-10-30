3224d8ec434892886823c720a049973c9745fe09
Shaker Hamdi <sheko.elanteko@gmail.com>
2015-10-30 21:13:05 +0200

    Added magnific popup and HEPCA projct

--- components/scripts/customScript.js
+++ components/scripts/customScript.js
@@ -33,4 +33,53 @@ $(document).ready(function() {
         });
     })(); //Navigation
 
+
+    (function() {
+        if ($("div.project a.more").length) {
+            $("div.project a.more").magnificPopup({
+                type: 'inline',
+                preloader: false,
+                focus: '#name',
+
+                removalDelay: 500, //delay removal by X to allow out-animation
+
+                // When elemened is focused, some mobile browsers in some cases zoom in
+                // It looks not nice, so we disable it:
+                callbacks: {
+                    beforeOpen: function() {
+
+                        if($(window).width() < 700) {
+                            this.st.focus = false;
+                        } else {
+                            this.st.focus = '#name';
+                        }
+
+                        this.st.mainClass = this.st.el.attr('data-effect');
+                    },
+                    open: function() {
+                        if($("ul.owlCarousel").length) {
+                            var owl = $("ul.owlCarousel");
+
+                            owl.owlCarousel({
+                                loop: false,
+                                items:1,
+                                margin: 15,
+                                responsiveClass: true,
+                                dots: true,
+                                nav: false,
+                                autoHeight:false
+                            });
+                        }
+                    },
+                    afterClose: function() {
+                        $("ul.owlCarousel").trigger('destroy.owl.carousel').removeClass('owl-carousel owl-loaded');
+                        $("ul.owlCarousel").find('.owl-stage-outer').children().unwrap();
+                    }
+                },
+
+                midClick: true // allow opening popup on middle mouse click. Always set
+            });
+        }
+    })(); //productLink
+
 });
