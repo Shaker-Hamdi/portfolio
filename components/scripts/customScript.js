733c4d34b245f4fd0d7cce86e5776db134865dc0
Shaker Hamdi <sheko.elanteko@gmail.com>
2015-10-30 19:47:27 +0200

    Finished the page

--- components/scripts/customScript.js
+++ components/scripts/customScript.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
 
-		
+	(function() {
+        var backToTop = $('a.backToTop');
+
+        backToTop.on('click', function(event) {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 300);
+
+            event.preventDefault();
+        });
+
+        $(window).on('scroll', function() {
+            var self = $(this),
+                height = self.height() / 8,
+                top = self.scrollTop();
+
+            if (top > height) {
+                if (!backToTop.is(':visible')) {
+                    backToTop.show();
+                }
+            } else {
+                backToTop.hide();
+            }
+        });
+    })(); //Back to top button
+
+
 });
